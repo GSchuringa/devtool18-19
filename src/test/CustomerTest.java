d97285d23ee8da60835e9ee75e2e452f19f4797e
Kris <Krisscholte@hotmail.com>
2018-07-01 16:30:17 +0200

    Added test or equals

--- src/test/CustomerTest.java
+++ src/test/CustomerTest.java
@@ -6,6 +6,11 @@ public class CustomerTest {
 
     @Test
     public void equals() {
+        Customer cust1 = new Customer("Kris");
+        Customer cust2 = new Customer("Kris");
+        Customer copyCust1 = cust1;
+        assertTrue(cust1.equals(cust2));
+        assertTrue(cust1.equals(copyCust1));
     }
 
     @Test
